5100ff50c9bd787b7a68d32030c0a1ed40f7086f
jk <jk@b0c1b02c-7ba7-4df7-b273-855bf36df2ab>
2008-11-22 18:23:35 +0000

    added on-click
    git-svn-id: https://cle.sk/repos/pub/cpan/Chart-OFC2/trunk@1088 b0c1b02c-7ba7-4df7-b273-855bf36df2ab

--- lib/Chart/OFC2/Element.pm
+++ lib/Chart/OFC2/Element.pm
@@ -9,6 +9,7 @@ has 'type_name'    => (is => 'rw', isa => enum(['bar', 'bar_filled', 'pie']), re
 has 'values'       => (is => 'rw', isa => 'ArrayRef', trigger => sub { $_[0]->extremes->reset('y' => $_[1]); } );
 has 'extremes'     => (is => 'rw', isa => 'Chart-OFC2-Extremes',  default => sub { Chart::OFC2::Extremes->new() }, lazy => 1);
 has 'use_extremes' => (is => 'rw', isa => 'Bool',  default => 1 );
+has 'on-click'     => (is => 'rw', isa => 'Str', );
 
 
 sub TO_JSON {
